3f1efef75b4d6ef921aeec2851b20cd2f352d7a7
rustem <prorusteg@gmail.com>
2022-11-23 19:37:04 +0300

    r

---
+++ Rabota3/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+int[] array = new int[8];
+void FillArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = new Random().Next(2, 99);
+    }
+}
+void PrintArray(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.Write($"{arr[7]}");
+    Console.Write("]");
+
+}
+
+FillArray(array);
+PrintArray(array);
